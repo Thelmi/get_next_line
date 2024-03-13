40764af656d6cbf62689313269c87c686f190eff
Taha_Helmy <153909093+Thelmi@users.noreply.github.com>
2024-03-13 17:59:16 +0400

    edge cases and memory leaks fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,111 +6,15 @@
 /*   By: thelmy <thelmy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 20:32:22 by thelmy            #+#    #+#             */
-/*   Updated: 2024/03/04 21:05:24 by thelmy           ###   ########.fr       */
+/*   Updated: 2024/03/13 16:26:47 by thelmy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-// #ifndef BUFFER 
-# define BUFFER 2
-// #endif
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	if(!s)
-		return NULL;
-	while (s[i] || (unsigned char)c == '\0')
-	{
-		if ((unsigned char)c == (unsigned char)s[i])
-			return ((char *)(s + i));
-		i++;
-	}
-	return (NULL);
-}
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t		i;
-	char		*destin;
-	char const	*sourc;
-
-	if (!dst && !src)
-		return (NULL);
-	destin = (char *)dst;
-	sourc = (char *)src;
-	i = 0;
-	while (i < n)
-	{
-		destin[i] = sourc[i];
-		i++;
-	}
-	return (dst);
-}
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	char	*d;
-	char	*s;
-
-	d = (char *)dst;
-	s = (char *)src;
-	if (dst > src)
-	{
-		while (len--)
-			d[len] = s[len];
-	}
-	else
-		ft_memcpy (dst, src, len);
-	return (dst);
-}
-
-size_t	ft_strlen(char *str)
-{
-	size_t i;
-	
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-		return (i);
-}
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
-		return (NULL);
-	while (s1 && s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] && s2[j] != '\n')
-		str[i++] = s2[j++];
-	if (s2[j] == '\n')
-		str[i++] = '\n';
-	str[i] = '\0';
-	if(s1)
-		free(s1);
-	return (str);
-}
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
-	static char	buffer[BUFFER + 1];
+	static char	buffer[BUFFER_SIZE + 1];
 	char		*str;
 	int			i;
 	int 		bytes;
@@ -118,13 +22,15 @@ char *get_next_line(int fd)
 	i = 0;
 	bytes = 1;
 	str = NULL;
+	if (fd < 0)
+		return (NULL);
 	while (!ft_strchr(str , '\n') && bytes > 0)
 	{
-		if (buffer && buffer[0] != '\0')
+		if (buffer[0] != '\0')
 		{
 			str = ft_strjoin(str,buffer);
-		while (buffer[i] && buffer[i] != '\n')
-			i++;
+			while (buffer[i] && buffer[i] != '\n')
+				i++;
 			if (buffer[i] == '\n')
 				ft_memmove(buffer,buffer + i + 1, ft_strlen(buffer + i + 1) + 1);
 			else
@@ -133,7 +39,7 @@ char *get_next_line(int fd)
 		else if (buffer[0] == '\0')
 		{
 			i = 0;
-			bytes = read(fd, buffer, BUFFER);
+			bytes = read(fd, buffer, BUFFER_SIZE);
 			if (bytes == -1)
 			{
 				// free(buffer);
@@ -152,37 +58,47 @@ char *get_next_line(int fd)
 				ft_memmove(buffer,buffer + i + 1, ft_strlen(buffer + i + 1) + 1);
 			else
 				buffer[0] = '\0';
+		}
 	}
-}
  	return (str);
 }
 
-int main()
-{
-	char *s = NULL;
-	int fd = open("text.txt", O_RDONLY);
-	// while (1)
-	// {
-	// 	s = get_next_line(fd);
-	// 	if (s)
-	// 	{
-	// 		printf("%s", s);
-	// 		free(s);
-	// 	}
-	// 	else if (!s)
-	// 		break ;
-	// }
-	int i = 0;
-	while (i < 15)
-	{
-		s = get_next_line(fd);
-		printf("%s", s);
-		if (s)
-			free(s);
-		i++;
-	}
-	// s = get_next_line(fd);
-	// printf("%p", s);
-}	
+// int main()
+// {
+// 	char *s = NULL;
+// 	int fd = open("text.txt", O_RDONLY);
+// 	// while (1)
+// 	// {
+// 	// 	s = get_next_line(fd);
+// 	// 	if (s)
+// 	// 	{
+// 	// 		printf("%s", s);
+// 	// 		free(s);
+// 	// 	}
+// 	// 	else if (!s)
+// 	// 		break ;
+// 	// }
+// 	int i = 0;
+// 	while (i < 15)
+// 	{
+// 		s = get_next_line(fd);
+// 		printf("%s", s);
+// 		if (s)
+// 			free(s);
+// 		i++;
+// 	}
+// 	// s = get_next_line(fd);
+// 	// printf("%p", s);
+// }
 
-
+// int main(int ac, char **av)
+// {
+// 	int fd = open(av[1], O_RDONLY);
+// 	char *str;
+// 	while ((str = get_next_line(fd)))
+// 	{
+// 		printf("%s", str);
+// 		free(str);
+// 	}
+// 	(void)ac;
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,17 +1,99 @@
-# include <stdlib.h>
-# include <unistd.h>
-# include <limits.h>
+#include "get_next_line.h"
 
-// char	*ft_strchr(const char *s, int c)
-// {
-// 	int	i;
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
 
-// 	i = 0;
-// 	while (s[i] || (unsigned char)c == '\0')
-// 	{
-// 		if ((unsigned char)c == (unsigned char)s[i])
-// 			return ((char *)(s + i));
-// 		i++;
-// 	}
-// 	return (NULL);
-// }
+	i = 0;
+	if(!s)
+		return NULL;
+	while (s[i] || (unsigned char)c == '\0')
+	{
+		if ((unsigned char)c == (unsigned char)s[i])
+			return ((char *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t		i;
+	char		*destin;
+	char const	*sourc;
+
+	if (!dst && !src)
+		return (NULL);
+	destin = (char *)dst;
+	sourc = (char *)src;
+	i = 0;
+	while (i < n)
+	{
+		destin[i] = sourc[i];
+		i++;
+	}
+	return (dst);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*d;
+	char	*s;
+
+	d = (char *)dst;
+	s = (char *)src;
+	if (dst > src)
+	{
+		while (len--)
+			d[len] = s[len];
+	}
+	else
+		ft_memcpy (dst, src, len);
+	return (dst);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t i;
+	
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+		return (i);
+}
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	int k = 0;
+	while (s2[k] && s2[k] != '\n')
+		k++;
+	if (s2[k] == '\n')
+		k++;
+	str = malloc(sizeof(char) * (ft_strlen(s1) + k + 1));
+	if (!str)
+	{
+		free(s1);
+		return (NULL);
+	}
+	while (s1 && s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] && s2[j] != '\n')
+		str[i++] = s2[j++];
+	if (s2[j] == '\n')
+		str[i++] = '\n';
+	str[i] = '\0';
+	if(s1)
+		free(s1);
+	return (str);
+}
